f7cce3f5c3beaaedfa9fb5576df680e7d09c1740
tcmalware <genarodvo@gmail.com>
2022-05-18 23:30:49 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio06/Ejercicio06/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio06
+{
+    public partial class App : Application
+    {
+        public static float ScreenHeight { get; set; }
+        public static float ScreenWidth { get; set; }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Ejercicio06/Ejercicio06/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio06
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new TapDemo());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new PinchDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new PanDemo());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new SwipeDemo());
+            };
+        }
+    }
+}
